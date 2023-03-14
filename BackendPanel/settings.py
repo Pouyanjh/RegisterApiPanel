63bddd12cb60aa790af623bd79fcaf216ac27b01
unknown <pouyanmelo@gmail.com>
2023-03-14 17:53:18 +0330

    second commit

--- BackendPanel/settings.py
+++ BackendPanel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ihzax94!17bs-)+xmw2n2gm2alj$vrxzg^)ktaustzwzd&^y0a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'User',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 AUTH_USER_MODEL = 'User.user'
 
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'BackendPanel.urls'
@@ -119,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
